840a0ac7e85a7d9c4859d751e53990919233e0da
devopsmahesh123 <devops.maheshbabu@gmail.com>
2025-09-08 13:04:08 +0530

    tests File

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    // Add custom rules here
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
